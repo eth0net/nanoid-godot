36f1bddb2f49bdbf5f9b85a0ca3f877531d4f584
Rihards Paskausks <paskausks@protonmail.com>
2026-02-12 12:23:31 +0200

    Fix autoload usage and pass size to with_alphabet (#1)
    Thanks @paskausks!

--- addons/nanoid/nanoid.gd
+++ addons/nanoid/nanoid.gd
@@ -1,5 +1,7 @@
+extends Node
+
 static func generate(size: int = NanoIDGenerator.DEFAULT_SIZE) -> String:
-	return with_alphabet(NanoIDAlphabets.URL)
+	return with_alphabet(NanoIDAlphabets.URL, size)
 
 
 static func with_alphabet(alphabet: String, size: int = NanoIDGenerator.DEFAULT_SIZE) -> String:
